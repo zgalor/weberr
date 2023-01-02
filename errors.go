daed1d5b44659f7434bb5bd8d601cc09ade14940
oisakov <oisakov@redhat.com>
2023-01-02 11:43:14 +0200

    fix is err

--- errors.go
+++ errors.go
@@ -396,9 +396,3 @@ func As(err error, target interface{}) bool {
 func Is(err error, target error) bool {
 	return goerrors.Is(err, target)
 }
-
-// Is finds the first error in err's chain that matches target,
-// and if so, sets target to that error value and returns true. Otherwise, it returns false.
-func Is(err error, target interface{}) bool {
-	return errors.Is(err, target)
-}
